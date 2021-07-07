a777b1a4bff072859b0795bc465fbefb428e9d9a
Matthew <cameron.matt@outlook.com>
2021-07-07 14:54:30 +1000

    have a working authentication

--- src/App.js
+++ src/App.js
@@ -2,15 +2,83 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import Home from './components/Home'
 import Dashboard from './components/Dashboard'
+import axios from 'axios';
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    }
+
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  checkLoginStatus() {
+    axios.get("http://localhost:3001/logged_in", { withCredentials: true })
+    .then(response => {
+      if (response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "LOGGED_IN",
+          user: response.data.user
+        })
+      } else if(!response.data.logged_in & this.state.loggedInStatus === "LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN",
+          user: {}
+        })
+      }
+    })
+    .catch(error => {
+      console.log("catch login error", error);
+    });
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+
+  handleLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    })
+  }
+
+  handleLogin(data) {
+    this.setState({
+      loggedInStatus: "LOGGED_IN",
+      user: data.user
+    });
+  }
+
   render() {
     return (
       <div className='app'>
         <BrowserRouter>
           <Switch>
-            <Route exact path={"/"} component={Home} />
-            <Route exact path={"/dashboard"} component={Dashboard} />
+            <Route 
+              exact path={"/"} 
+              render={props => (
+                <Home 
+                  {...props} 
+                  handleLogout={this.handleLogout}
+                  handleLogin={this.handleLogin}
+                  loggedInStatus={this.state.loggedInStatus} 
+                  />
+              )} />
+            <Route 
+              exact path={"/dashboard"} 
+              render={props => (
+                <Dashboard 
+                  {...props} 
+                  loggedInStatus={this.state.loggedInStatus} 
+                  />
+              )} />
           </Switch>
         </BrowserRouter>
       </div>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Home extends Component {
-    render() {
-        return (
+const Dashboard = props => {
+    return (
+        <div>
             <div>
-                <h1>Home</h1>
-                <h1>Home</h1>
+                <h1>Dashboard</h1>
+                <h1>Status: {props.loggedInStatus}</h1>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+export default Dashboard;
