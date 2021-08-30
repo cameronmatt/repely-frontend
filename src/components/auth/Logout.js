8f2825fdacacd565a40ae91f047700eecf5c17e6
Matthew <cameron.matt@outlook.com>
2021-08-31 09:25:53 +1000

    Now able to logout, and destroy jwt

--- src/components/auth/Logout.js
+++ src/components/auth/Logout.js
@@ -1,19 +1,44 @@
 import React, { Component } from "react";
 import axios from 'axios';
-//import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 
-// const Logout = (props) => {
+const Logout = (props) => {
 
-//     //this.handleLogoutClick = this.handleLogoutClick.bind(this);
-//     console.log("WHAT IS LOGOUT PROPS", props.currentUser)
-//     //const history = useHistory();
+    //this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    console.log("WHAT IS LOGOUT PROPS", props.currentUser)
+    const history = useHistory();
 
-//       const handleLogoutClick = () => {
-//           axios.delete(`http://localhost:3001/users/${props.currentUser[0].user.user_id}`)
+      const handleLogoutClick = () => {
+        localStorage.removeItem("jwt");
+        history.push('/')
+      }
+      
+
+    return (
+        <div>
+            <Button onClick={() => handleLogoutClick()}>Logout</Button>
+        </div>
+    );  
+}
+
+export default Logout; 
+
+// class Logout extends Component {
+
+//     constructor(props) {
+//         super(props);
+
+//         this.handleLogoutClick = this.handleLogoutClick.bind(this);
+//         console.log("WHAT IS LOGOUT PROPS", this.props)
+//       }
+
+//       handleLogoutClick() {
+//           axios
+//           .delete(`http://localhost:3001/users/${this.props.currentUser.id}`)
 //           .then(response => {
-//               props.history.push('/')
+//               this.props.handleLogout();
 //           })
 //           .catch(error => {
 //               console.log("logout error", error)
@@ -21,43 +46,13 @@ import { Button } from 'react-bootstrap';
 //       }
       
 
-//     return (
-//         <div>
-//             <Button onClick={() => handleLogoutClick()}>Logout</Button>
-//         </div>
-//     );  
+//     render() {
+//         return (
+//             <div>
+//                 <Button onClick={() => this.handleLogoutClick()}>Logout</Button>
+//             </div>
+//         );
+//     }
 // }
 
 // export default Logout; 
-
-class Logout extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        console.log("WHAT IS LOGOUT PROPS", this.props)
-      }
-
-      handleLogoutClick() {
-          axios
-          .delete(`http://localhost:3001/users/${this.props.currentUser.id}`)
-          .then(response => {
-              this.props.handleLogout();
-          })
-          .catch(error => {
-              console.log("logout error", error)
-          })
-      }
-      
-
-    render() {
-        return (
-            <div>
-                <Button onClick={() => this.handleLogoutClick()}>Logout</Button>
-            </div>
-        );
-    }
-}
-
-export default Logout; 
