3fec85b38cfdfd9186335abdd4e6411d34deb8a5
Matthew <cameron.matt@outlook.com>
2021-09-19 22:14:04 +1000

    Clean up App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import Home from './components/Home'
-import Login from "./components/auth/Login";
 import Dashboard from './components/Dashboard'
-import axios from 'axios';
 import AddTicket from './components/AddTicket';
 import Logout from "./components/auth/Logout";
 import Registration from "./components/auth/Registration";
-import { connect } from 'react-redux';
-import { findUser } from './redux/userSlice';
 
 class App extends Component {
 
@@ -18,50 +14,7 @@ class App extends Component {
     this.state = {
       //loggedInStatus: "NOT_LOGGED_IN",
     }
-
-    // this.handleLogin = this.handleLogin.bind(this);
-    // this.handleLogout = this.handleLogout.bind(this);
   }
-
-  // checkLoginStatus() {
-  //   axios.get("http://localhost:3001/logged_in", { withCredentials: true })
-  //   .then(response => {
-  //     if (response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN") {
-  //       this.setState({
-  //         loggedInStatus: "LOGGED_IN",
-  //         user: response.data.user
-  //       })
-  //     } else if(!response.data.logged_in & this.state.loggedInStatus === "LOGGED_IN") {
-  //       this.setState({
-  //         loggedInStatus: "NOT_LOGGED_IN",
-  //         user: {}
-  //       })
-  //     }
-  //   })
-  //   .catch(error => {
-  //     console.log("catch login error", error);
-  //   });
-  // }
-
-  // componentDidMount() {
-  //   this.checkLoginStatus();
-  // }
-
-  // handleLogout() {
-  //   this.setState({
-  //     loggedInStatus: "NOT_LOGGED_IN",
-  //     user: {}
-  //   })
-  // }
-
-  // handleLogin(data) {
-  //   console.log('WHAT IS THIS DATAAAA', data)
-  //   this.setState({
-  //     loggedInStatus: "LOGGED_IN",
-  //     user: data.user
-  //   });
-    
-  // }
 
   render() {
     return (
